338b780c594fb7d0f299c55d2b98711722f756d6
Edwin Richard Idan <edwinidan07@gmail.com>
2026-07-07 22:41:38 +0000

    Remove first image from hero carousel

--- components/HeroCarousel.jsx
+++ components/HeroCarousel.jsx
@@ -4,7 +4,6 @@ import { useState, useEffect } from "react";
 import Image from "next/image";
 
 const images = [
-  { src: "/hero-images/image 3.webp", position: "object-center" },
   { src: "/hero-images/image 24.webp", position: "object-top" }, // Often better for faces/people
   { src: "/hero-images/image 25.webp", position: "object-center" },
   { src: "/hero-images/image 14.webp", position: "object-center" },
